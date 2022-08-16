a6d0ee16b2b19c83c5a7755188dea472119f5c7e
G-dro <Argentum2021_spb@mail.ru>
2022-08-16 23:19:41 +0300

    Beer

--- papp.py
+++
@@ -1,6 +0,0 @@
-import os
-file_path=str(input('Введите путь папки: '))
-number=str(input('Введите входящий номер раствора:'))
-file_path=file_path.replace('\\','\\\\')
-os.chdir(file_path)
-os.mkdir(number)
